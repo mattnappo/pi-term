0866e31da9ede5ab4a0ab15f47058496fa5d89b3
xoreo <mattnappo@gmail.com>
2019-02-22 22:30:43 -0500

    Starting new branch

--- src/server/helpers/install.sh
+++ src/server/helpers/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+ROOTDIR="$HOME/git/pi-term"
+
 if [ -d "$DIRECTORY" ]; then # Git pull if the repo already is cloned
-    cd ~/git/pi-term
+    cd $ROOTDIR
     git pull origin master
 
 else # Get the repo if it doesn't exist
@@ -11,12 +13,12 @@ else # Get the repo if it doesn't exist
     git clone https://github.com/xoreo/pi-term.git
     
     # Make the helpers directory
-    mkdir -p ~/git/pi-term/helpers
-    cd ~/git/pi-term/helpers
+    mkdir -p $ROOTDIR/helpers
+    cd $ROOTDIR/helpers
 fi
 
-cd ~/git/pi-term
+cd $ROOTDIR
 npm i
 
 # Move the scripts
-cp ../src/server/helpers/* .
+cp $ROOTDIR/src/server/helpers/* $ROOTDIR/helpers
