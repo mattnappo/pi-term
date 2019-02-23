1c4b59f982104d1ff6392e185e98d108ddc87a0c
xoreo <mattnappo@gmail.com>
2019-02-22 22:11:11 -0500

    Finished docker helper

--- src/server/scripts/docker.js
+++ src/server/scripts/docker.js
@@ -10,13 +10,53 @@ function sendCommandAndParse(command) {
     return parsed;
 }
 
-// The object containing the host's docker image data
-const data = {
-    repositories: sendCommandAndParse("docker images --format '{{json .Repository}}'"),
-    created: sendCommandAndParse("docker images --format '{{json .CreatedSince}}'")
-};
+// GetImageInfo - Get the image info and write to a file
+function GetImageInfo() {
+    let data = {
+        repositories: sendCommandAndParse("docker images --format '{{json .Repository}}'"),
+        created: sendCommandAndParse("docker images --format '{{json .CreatedSince}}'")
+    };
 
-// Write to file
-fs.writeFile("dockerpull.txt", JSON.stringify(data), function(err, data) {
-    if (err) console.log(err);
-});
+    // Write to file
+    // fs.writeFile("docker.image.info", JSON.stringify(data), function (err, data) {
+    //     if (err) console.log(err);
+    // });
+
+    return data;
+}
+
+// GetContinerInfo - Get the running container info and write to a file
+function GetContinerInfo() {
+    let data = {
+        id: sendCommandAndParse("docker ps --no-trunc --format '{{json .ID}}'"),
+        image: sendCommandAndParse("docker ps --format '{{json .Image}}'"),
+        status: sendCommandAndParse("docker ps --format '{{json .Status}}'")
+    };
+
+    // Write to file
+    // fs.writeFile("docker.container.info", JSON.stringify(data), function (err, data) {
+    //     if (err) console.log(err);
+    // });
+
+    return data;
+}
+
+// GetInfo - Get all of the docker info (images and containers)
+function GetInfo() {
+    let data = {
+        images: GetImageInfo(),
+        container: GetContinerInfo()
+    }
+    
+    // Write to file
+    fs.writeFile("docker.info", JSON.stringify(data), function (err, data) {
+        if (err) console.log(err);
+    });
+}
+
+// Export necessary functions
+module.exports = {
+    GetImageInfo: GetImageInfo,
+    GetContinerInfo: GetContinerInfo,
+    GetInfo: GetInfo
+};
---
+++ src/server/scripts/helper.js
@@ -0,0 +1,17 @@
+const docker = require("./docker");
+const cpu = require("./cpu");
+
+// I am aware that this is a really shitty solution to this whole problem
+if (process.argv[2] == "--dockerinfo") {
+    docker.GetInfo();
+    /*
+        To call (from express.js backend), do this:
+        // Make an init helpers func that installs latest code to helpers
+        execSync("node ~/git/pi-term/helpers/ && cat ~/git/pi-term/helpers/docker.info")
+    */
+
+} else if (process.argv[2] == "--cpuinfo") {
+    cpu.GetInfo();
+} else {
+    console.log("Invalid helper parameter. Options:\n--dockerinfo\n--cpuinfo");
+}
