d8cf63e83ed03b937536939d5048ef8310a6128e
xoreo <mattnappo@gmail.com>
2019-02-22 21:28:56 -0500

    Strip repos done

--- src/server/scripts/docker.js
+++ src/server/scripts/docker.js
@@ -1,28 +1,16 @@
 const execSync = require("child_process").execSync;
 
-// getRepos - Return an array of the host's docker containers' repository names
-function getRepos() {
-    let command = "docker images --format '{{json .Repository}}'";
+// sendCommandAndParse - Exec a command on the host and parse it accordingly
+function sendCommandAndParse(command) {
     let stdout = execSync(command).toString("utf8");
-    // console.log(stdout);
+    let stripped = stdout.replace(/"/g, "");
+    let parsed = stripped.split("\n");
+    return parsed;
 }
 
-// getCreated - Return an array of when the host's docker containers were created
-function getCreated() {
-    let command = "docker images --format '{{json .Created}}'";
-    let stdout = execSync(command).toString("utf8");
-    console.log(stdout.replace("\"", ""));
-}
+const data = {
+    repositories: sendCommandAndParse("docker images --format '{{json .Repository}}'"),
+    created: sendCommandAndParse("docker images --format '{{json .CreatedSince}}'")
+};
 
-function getAllJson() {
-    let command = "docker images --format '{{json .}}'";
-    let stdout = execSync(command).toString("utf8");
-    JSON.stringify(stdout, null, "\t"); // stringify with tabs inserted at each level
-    JSON.stringify(stdout, null, 4);    // stringify with 4 spaces at each level
-    console.log(stdout);
-}
-
-// getRepos();
-// getCreated();
-
-getAllJson();
+console.log(data);
